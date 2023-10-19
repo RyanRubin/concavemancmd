2ce22941f225eaf238bb7d222f07dac1404cb788
Ryan Rubin <dev.rrtechenterprise@gmail.com>
2023-10-19 23:38:06 +0800

    points from cmd

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const process = require('process');
 const concaveman = require('concaveman');
-const points = JSON.parse('[[10,20],[30,12.5]]');
-const polygon = concaveman(points);
-console.log(polygon);
+
+try {
+  const points = JSON.parse(process.argv[process.argv.length - 1]);
+  const polygon = concaveman(points);
+  console.log(polygon);
+} catch (ex) {
+  console.log('[]');
+}
